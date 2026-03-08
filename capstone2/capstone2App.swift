6c7f7d30e5486a2953f3c42efa5c79489e55712b
Alethea Liu <alethea.liu09@gmail.com>
2026-03-08 11:47:21 -0400

    Initial Commit

---
+++ capstone2/capstone2App.swift
@@ -0,0 +1,17 @@
+//
+//  capstone2App.swift
+//  capstone2
+//
+//  Created by Xiaojing Meng on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct capstone2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
